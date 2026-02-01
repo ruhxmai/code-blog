4dbf08581f0e3bbcc8bdda50d1584e61cac5214e
ruhxmai <typlq@mail.ru>
2026-02-01 14:35:25 +0500

    add github workflow to deploy app

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,13 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['./assets/css/main.css'],
+  
+  app: {
+    baseURL: '/code-blog/'
+  },
+  
+  ssr: false,
+
   modules: [
     '@nuxtjs/google-fonts',
     '@nuxt/content'
